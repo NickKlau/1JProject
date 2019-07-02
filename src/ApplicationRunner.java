05c7eef07441260da850a13c35b49ce0cecbc63c
Name <kolya.kalchuk@gmail.com>
2019-07-02 19:18:09 +0300

    Initial commit

---
+++ src/ApplicationRunner.java
@@ -0,0 +1,41 @@
+import java.util.Queue;
+
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.circle;
+
+public class ApplicationRunner {
+
+    Triangle triangle = new Triangle(base:12,heigh:15);
+
+    Quadrilateral quadrilateral = new Quadrilateral(side:10,base:20);
+
+    Shape:maxShape;
+
+    if(circle.gerArea()>square.getArea())
+
+    {
+        maxShape = circle;
+    }
+    else
+
+    {
+        maxShape = square;
+    }
+
+    if(maxShape.getArea() <triangle.
+
+    getArea())
+
+    {
+        maxShape triangle;
+    }
+
+    if(maxShape.getArea()) <quadrilateral.
+
+    getArea() {
+        maxShape = quadrilateral;
+    }
+
+
+    System.out.println("Max shape has area"+maxShape.getArea());
+
+}
